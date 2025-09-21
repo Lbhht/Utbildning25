6c7a4f88a5cfc55187d0f37e6265a10297b0b6e1
Martin <martin.wisell.java25@edu.edugrade.se>
2025-09-21 09:51:28 +0200

    lektion

---
+++ lecture_2/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World");
+        System.out.println(7);
+        System.out.println(7.23);
+        System.out.println('A');
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
