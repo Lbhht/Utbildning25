a497a0a8952868a0750634e0c4c9f89d5f4f3809
Martin <martin.wisell.java25@edu.edugrade.se>
2025-10-07 00:15:40 +0200

    La till attribut och metod till hero klassen

--- oop-assignment/src/characters/Hero.java
+++ oop-assignment/src/characters/Hero.java
@@ -1,12 +1,21 @@
 package characters;
 
 public class Hero extends Character{
+    int level;
+    int currentExp;
+
     public Hero(String name) {
         super(name);
+        this.level = 1;
+        this.currentExp = 0;
     }
 
     @Override
     public void saySomething() {
         System.out.println(name + " says something");
+    }
+
+    public void gainExp() {
+
     }
 }
